4c5d3a81ce57d7e27edc6c2604b3b2fb1a81499e
beautisha <korobochky@yandex.ru>
2022-08-03 16:34:58 +0300

    solve

--- src/index.js
+++ src/index.js
@@ -36,9 +36,33 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
+// 10 - .  11 - - 
 function decode(expr) {
-    // write your solution here
+    str = '';
+    for (let i = 0; i <expr.length; i = i + 10) {
+        for (let j = i; j < i+10; j = j+2) {
+            if (expr[j] == '*') {
+                str = str + ' ';
+                break;
+            }
+            switch (expr[j]+expr[j+1]) {
+                case '00': str = str + ''; break;
+                case '10': str = str + '.'; break;
+                case '11': str = str + '-'; break;
+            }
+        }
+        str = str + '#';
+    }
+    console.log (str);
+    let arr = str.split('#');
+    let arr1 = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == ' ') {
+            arr1[i-1] = arr1[i-1]+' ';
+        } else {
+        arr1[i] = MORSE_TABLE[arr[i]]};
+    }
+    return arr1.toString().replaceAll(',','');
 }
 
 module.exports = {
